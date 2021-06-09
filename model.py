72f25e1b5a3eddad1ae78f1ea724eb6ed788038d
ganjf <1194520007@qq.com>
2021-06-09 17:19:15 +0800

    baseline

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+import pdb
 import torch
 import torch.nn as nn
-import torch.optim as optim 
-import torch.nn.functional as F 
+import torch.nn.functional as F
+from dropblock import DropBlock3D, LinearScheduler
 from torch.utils.checkpoint import checkpoint
-from torch.nn import DataParallel
+
 config = {
     "act_fn": lambda: nn.LeakyReLU(0.1, inplace=True),
     "norm_fn": lambda c: nn.BatchNorm3d(num_features=c)
@@ -11,8 +13,7 @@ config = {
 
 def densenet3d(snapshot=None):
     model = DenseNet()
-    model = DataParallel(model)
-    
+
     if snapshot is None:
         initialize(model.modules())
         print("Random initialized")
@@ -21,15 +22,15 @@ def densenet3d(snapshot=None):
         model.load_state_dict(state_dict)
         print("loads weight from {}".format(snapshot))
 
-    return model 
+    return model
 
 
 def initialize(modules):
     for module in modules:
         if isinstance(module, nn.Conv3d) or isinstance(module, nn.ConvTranspose3d):
-            nn.init.kaiming_uniform_(module.weight, mode = "fan_in")
+            nn.init.kaiming_uniform_(module.weight, mode="fan_in")
         elif isinstance(module, nn.Linear):
-            nn.init.kaiming_uniform_(module.weight, mode = "fan_in")
+            nn.init.kaiming_uniform_(module.weight, mode="fan_in")
             module.bias.data.zero_()
 
 
@@ -43,23 +44,23 @@ class ConvBlock(nn.Sequential):
 
         self.in_channels = in_channels
         self.growth_rate = growth_rate
-        
+
         self.add_module("norm_1", norm_fn(in_channels))
         self.add_module("act_1", act_fn())
-        self.add_module("conv_1", nn.Conv3d(in_channels, bottleneck * growth_rate, kernel_size = 1, stride = 1, padding = 0, bias = True))
+        self.add_module("conv_1", nn.Conv3d(in_channels, bottleneck * growth_rate,
+                                            kernel_size=1, stride=1, padding=0, bias=True))
         self.add_module("norm_2", norm_fn(bottleneck * growth_rate))
         self.add_module("act_2", act_fn())
-        self.add_module("conv_2", nn.Conv3d(bottleneck * growth_rate, growth_rate, kernel_size = 3, stride = 1, padding = 1, bias = True))
-    
+        self.add_module("conv_2", nn.Conv3d(bottleneck * growth_rate, growth_rate,
+                                            kernel_size=3, stride=1, padding=1, bias=True))
 
     def forward(self, x):
         super_forward = super(ConvBlock, self).forward
         residual = x
-        #features = super_forward(x)
+        # features = super_forward(x)
         features = checkpoint(super_forward, x)
         out = torch.cat([residual, features], 1)
         return out
-
 
     @property
     def out_channels(self):
@@ -76,14 +77,15 @@ class TransmitBlock(nn.Sequential):
         assert in_channels % compression == 0
 
         self.in_channels = in_channels
-        self.compression = compression 
+        self.compression = compression
 
         self.add_module("norm", norm_fn(in_channels))
         self.add_module("act", act_fn())
 
         if not is_last_layer:
-            self.add_module("conv", nn.Conv3d(in_channels, in_channels // compression, kernel_size = 1, stride = 1, padding = 0, bias = True))
-            self.add_module("pool", nn.AvgPool3d(kernel_size = 2, stride = 2, padding = 0))
+            self.add_module("conv", nn.Conv3d(in_channels, in_channels // compression,
+                                              kernel_size=1, stride=1, padding=0, bias=True))
+            self.add_module("pool", nn.AvgPool3d(kernel_size=2, stride=2, padding=0))
         else:
             self.compression = 1
 
@@ -97,16 +99,24 @@ class DenseNet(nn.Module):
         super(DenseNet, self).__init__()
         input_channels = 1
         conv_channels = 32
-        down_structure = [2,2,2]
-        output_channels = 4#4#2
+        down_structure = [2, 2, 2]
+        output_channels = 4  # 2
         act_fn = config["act_fn"]
         norm_fn = config["norm_fn"]
         self.features = nn.Sequential()
-        self.features.add_module("init_conv", nn.Conv3d(input_channels, conv_channels, kernel_size = 3, stride = 1, padding = 1, bias = True))
+        self.features.add_module("init_conv", nn.Conv3d(input_channels, conv_channels,
+                                                        kernel_size=3, stride=1, padding=1, bias=True))
         self.features.add_module("init_norm", norm_fn(conv_channels))
         self.features.add_module("init_act", act_fn())
+        self.dropblock = LinearScheduler(
+            DropBlock3D(drop_prob=0., block_size=5),
+            start_value=0.,
+            stop_value=0.5,
+            nr_steps=5000
+        )
 
         channels = conv_channels
+        self.features.add_module('drop_block', DropBlock3D(drop_prob=0.1, block_size=5))
 
         for i, num_layers in enumerate(down_structure):
             for j in range(num_layers):
@@ -114,23 +124,20 @@ class DenseNet(nn.Module):
                 self.features.add_module("block{}_layer{}".format(i + 1, j + 1), conv_layer)
                 channels = conv_layer.out_channels
 
-            # dowmsample 
-            trans_layer = TransmitBlock(channels, is_last_layer = (i == len(down_structure) - 1))
+            # down-sample
+            trans_layer = TransmitBlock(channels, is_last_layer=(i == len(down_structure) - 1))
             self.features.add_module("transition{}".format(i + 1), trans_layer)
-            channels = trans_layer.out_channels       
+            channels = trans_layer.out_channels
 
         self.classifier = nn.Linear(channels, output_channels)
 
-    
     def forward(self, x, **return_opts):
+        self.dropblock.step()
         batch_size, _, z, h, w = x.size()
 
-        features = self.features(x)
-        # print("features", features.size())
+        features = self.dropblock(self.features(x))
         pooled = F.adaptive_avg_pool3d(features, 1).view(batch_size, -1)
-        # print("pooled", pooled.size())
         scores = self.classifier(pooled)
-        # print("scored", scores.size())
 
         if len(return_opts) == 0:
-            return scores 
+            return scores
--- test_case_roc.py
+++ test_case_roc.py
@@ -1,17 +1,14 @@
+import os
 import torch
 import numpy as np 
-import data 
-from data import TestDataset
-from model import densenet3d
-import csv
 import torch.nn.functional as F
-from torch.nn import DataParallel
 from torch.utils.data import DataLoader
-from sklearn.metrics import roc_auc_score
 from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score, confusion_matrix
 from logger import *
-import os 
-os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+from data_raw import TestDataset
+from model import densenet3d
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0, 1"
 
 def results(labels, preds, avg_type, class_type = "other"):
     precision = precision_score(labels, preds, average = avg_type)
@@ -26,6 +23,10 @@ def results(labels, preds, avg_type, class_type = "other"):
     if class_type == "two":
         return recall
 
+def softmax(x):
+    exp_x = np.exp(x)
+    softmax_x = exp_x / np.sum(exp_x)
+    return softmax_x 
 
 class Prediction:
     def __init__(self, outputs, labels, path_name, patient_id):
@@ -34,7 +35,6 @@ class Prediction:
         self.path_name = path_name
         self.patient_id = patient_id
 
-    
     def __eq__(self, other):
         if self.patient_id == other.pateint_id: #and self.age == other.age:
             return True
@@ -49,47 +49,36 @@ class Prediction:
 
 
 def test(test_data_loader, model, patient_id_list):
-    predict_value = []
-    names_box_test = []
     print(len(test_data_loader))
     predictions = []
 
-    pred_two, label_two, pred_four, label_four = [], [], [], []
     for index, (inputs, labels, patient_name, patient_ids) in enumerate(test_data_loader):
         model.eval()
         inputs = inputs.cuda()
         labels = labels.cuda()
-        # forward 
         inputs = inputs.unsqueeze(dim=1).float()
         inputs = F.interpolate(inputs, size=[16, 128, 128], mode = "trilinear", align_corners = False)
         outputs = model(inputs)
-        # pred_six = torch.max(outputs, 1)[1].cpu().numpy().squeeze()
         labels_array = labels.cpu().numpy()
         outputs_array = outputs.detach().cpu().numpy()
-        # import ipdb; ipdb.set_trace()
         for index, patient_id in enumerate(patient_ids):
             patient_id = str(patient_id.cpu().numpy().item())
             if patient_id in patient_id_list:
                 prediction = Prediction(outputs_array[index], labels_array[index], patient_name[index], patient_id)
                 predictions.append(prediction)
-    # import ipdb; ipdb.set_trace()
     return predictions
 
 
 def gen_dict(pred): 
-    # import ipdb; ipdb.set_trace()
     pred_sorted = sorted(pred)
     pred_lists = [[pred_sorted[0]]]
     for i in range(1, len(pred_sorted)):
         cur_info = pred_sorted[i]
         pre_info = pred_sorted[i - 1]
-        # import ipdb;ipdb.set_trace()
         if cur_info.patient_id != pre_info.patient_id:
             pred_lists.append([cur_info])
         else:
             pred_lists[-1].append(cur_info)
-    
-    # import ipdb;ipdb.set_trace()
     return pred_lists 
 
 
@@ -136,23 +125,12 @@ def gen_four_class(preds, labels):
             pred_four.append(1)
         else:
             pred_four.append(pred)
-    # import ipdb; ipdb.set_trace()
 
     return pred_four, label_four
 
 
 def test_case(test_data_loader, model):
-    # patient_ids = gen_ids("./utils/patients_id_test.csv")
-    patient_ids = []
-    patient_names = []
-    with open("./utils/valid_clean_fold.csv", "r") as fin:
-        for line in fin:
-            # path_name, four_label, six_label, patient_id, patient_name, gender, age = line.strip().split(',')
-            name, four_label, six_label, patient_id, patient_name, gender, age, score, most_score = line.strip().split(',')
-            if patient_name not in patient_names:
-                patient_names.append(patient_name)
-                patient_ids.append(patient_id)
- 
+    patient_ids = gen_ids("./utils/patients_id_test.csv")
     preds = test(test_data_loader, model, patient_ids)
     pred_lists = gen_dict(preds)
     case_preds, case_labels, case_ids, case_path = [], [], [], []
@@ -176,10 +154,6 @@ def test_case(test_data_loader, model):
     pred_six, label_six = case_preds, case_labels
     box_train = zip(case_path, case_ids, label_six, pred_six, label_four, pred_four, label_two, pred_two)
     
-    # with open('case_test_six_{}.csv'.format(epoch),'w') as result_file:
-    #     wr = csv.writer(result_file)
-    #     for row in box_train:
-    #         wr.writerow(row)
     results(label_six, pred_six, avg_type = "macro", class_type = "six")
     results(label_six, pred_six, avg_type = "micro", class_type = "six")
     results(label_four, pred_four, avg_type = "macro", class_type = "four")
@@ -190,17 +164,17 @@ def test_case(test_data_loader, model):
 
 if __name__ == "__main__":
     data_test = TestDataset()
-    test_data_loader = DataLoader(dataset = data_test, batch_size = 36, shuffle = False, num_workers = 48)
+    test_data_loader = DataLoader(dataset = data_test, batch_size = 4, shuffle = False, num_workers = 16)
     logfile = "./test.log"
     sys.stdout = Logger(logfile)
     patient_ids = gen_ids("./utils/patients_id_test.csv")
-    for epoch in [20]:#[10,15,20,25,30,35,40,45]:#[30, 35, 40]:
+    
+    for epoch in [5, 10, 15,20,25,30,35,40,45,55,60,65,70,75,80,85,90,95,100]:
         print("epoch:{}".format(epoch))
-        PATH = "./checkpoint/{}_epoch.pth".format(epoch) 
-        # sets = parse_opts() 
-        # sets.phase = "test"
+        PATH = "xxxx{}.pth".format(epoch) 
         checkpoint = torch.load(PATH)
         model = densenet3d().cuda()
+        model = torch.nn.DataParallel(model)
         model.load_state_dict(checkpoint)
         preds = test(test_data_loader, model, patient_ids)
         pred_lists = gen_dict(preds)
@@ -208,6 +182,7 @@ if __name__ == "__main__":
         case_labels = []
         case_ids = []
         case_path = []
+        pred_probs = []
         for case_pred in pred_lists:
             seq_preds = []
             for seq_pred in case_pred:
@@ -215,9 +190,9 @@ if __name__ == "__main__":
                 label = seq_pred.labels
                 patient_id = seq_pred.patient_id
                 path_name = seq_pred.path_name
-            # import ipdb;ipdb.set_trace()
-            
             mean_pred = np.mean(seq_preds,0)
+            pred_prob = softmax(mean_pred)
+            pred_probs.append(pred_prob.tolist())
             type_pred = np.argmax(mean_pred)
             case_preds.append(type_pred)
             case_ids.append(patient_id)
@@ -228,11 +203,7 @@ if __name__ == "__main__":
         pred_four, label_four = gen_four_class(case_preds, case_labels)
         pred_six, label_six = case_preds, case_labels
         box_train = zip(case_path, case_ids, label_six, pred_six, label_four, pred_four, label_two, pred_two)
-        
-        with open('case_test_four_norm{}.csv'.format(epoch),'w') as result_file:
-            wr = csv.writer(result_file)
-            for row in box_train:
-                wr.writerow(row)
+
         results(label_six, pred_six, avg_type="macro")
         results(label_six, pred_six, avg_type="micro")
         results(label_four, pred_four, avg_type="macro")
@@ -240,10 +211,21 @@ if __name__ == "__main__":
         results(label_two, pred_two, avg_type="macro", class_type = "two")
 
 
-        
+    
+
 
     
-        
+
+
+
+
+
+
+
+    
+
+
+    
 
 
 
